f2f918c41292c3da3127dc7348104e3d4f599294
HectorM5 <13919506-HectorM5@users.noreply.replit.com>
2024-02-01 13:24:18 +0000

    Changed a lot of things, made it look better and made it so when you go on the other HTML pages that you can get back to the index.html and all the other HTML pages

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function myFunction(){
+function myFunction() {
   var x = document.getElementById("About me");
   var i = x.selectedIndex;
   var b = x.value;
-  document.getElementById("Hector").innerHTML = x.options[i].text;
-  document.getElementById("Hector's").innerHTML = "Hector's"
-  document.getElementById("postcard").style.backgroundImage = "url('img/"+ b +"@2x.jpg"
+  document.getElementById("Hector").innerHTML;
+  document.getElementById("Hector's").innerHTML;
+  document.getElementById("postcard").style.backgroundImage = "url('img/" + b + "@2x.jpg"
 }
